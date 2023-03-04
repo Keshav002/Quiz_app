2de39f77c170f4700edc9151772eb8c4e4265650
Keshav <keshavtayal002@gmail.com>
2023-03-04 16:44:05 +0530

    final commit

---
+++ backend/database/data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        id: 1,
+        question : "When was Enverus India Private Limited incorporated?",
+        options : [
+            '2005',
+            '2006',
+            '2007',
+        ]
+    },
+    {
+        id: 2,
+        question : "Which type of platform or services does Enverus provides?",
+        options : [
+            'Energy driven SaaS platform',
+            'Hardware Solutions',
+            'Social Media platform',
+        ]
+    },
+    {
+        id: 3,
+        question : "Who is the CEO of Enverus",
+        options : [
+            'Jeff Hughes',
+            'Sundar Pichai',
+            'Mark Zukerberg',
+        ]
+    },
+    {
+        id: 4,
+        question : "Where is Enverus India office",
+        options : [
+            'Banglore',
+            'Gurugram',
+            'Pune'
+        ]
+    },
+    {
+        id: 5,
+        question : "Enverus is one of the leading publisher of which Database?",
+        options : [
+            'Oil and Gas',
+            'Solar Cells',
+            'Energy Production',
+        ]
+    }
+];
+
+export const answers = [2, 1, 2, 1, 1];
